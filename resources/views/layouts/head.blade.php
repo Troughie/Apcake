aa950aaf616a7e18128c5b27425c636c7183adff
Troughie <nguyentienngoc7166@gmail.com>
2023-04-06 02:08:03 +0700

    something

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,4 +1,3 @@
-
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -35,12 +34,9 @@
 <link href="{{ asset('vendors/nice-select/css/nice-select.css') }}" rel="stylesheet">
 
 <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-<<<<<<< HEAD
 <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
-=======
 <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
->>>>>>> something
